fd0eaa9e23357842a80ea6443d27dd9e56f47368
fhidesjoseph <fhidesjoseph@gmail.com>
2022-06-17 18:48:20 +0300

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,57 @@
 /* Your Code Here */
+let createEmployeeRecord = row=>{
+    return {
+        firstName: row[0],
+        familyName: row[1],
+        title: row[2],
+        payPerHour: row[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+let createEmployeeRecords = employeeRowData=> {
+    return employeeRowData.map(function(row){
+        return createEmployeeRecord(row)
+    })
+}
+let createTimeInEvent = dateStamp=>{
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+let createTimeOutEvent = dateStamp=>{
+    let [date, hour] = dateStamp.split(' ')
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(hour, 10),
+        date,
+    })
+
+    return this
+}
+let hoursWorkedOnDate = dateSort=>{
+    let inEvent = this.timeInEvents.find(function(e){
+        return e.date === dateSort
+    })
+
+    let outEvent = this.timeOutEvents.find(function(e){
+        return e.date === dateSort
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+let wagesEarnedOnDate = dateSort=>{
+    let rawWage = hoursWorkedOnDate.call(this, dateSort)
+        * this.payPerHour
+    return parseFloat(rawWage.toString())
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +74,14 @@ const allWagesFor = function () {
     return payable
 }
 
+let findEmployeeByFirstName = (srcArray, firstName)=> {
+    return srcArray.find(function(record){
+      return record.firstName === firstName
+    })
+  }
+
+  let calculatePayroll = (arrayOfEmployeeRecords)=>{
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+} 
